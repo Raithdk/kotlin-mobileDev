07d339c532845de0342c4b0ca6d5a3367bd8638c
Thomas <Raithdk@gmail.com>
2022-10-10 16:53:26 +0200

    App commit

---
+++ app/src/main/java/com/example/myfirstkotlinapp/data/MovieDatabase.kt
@@ -0,0 +1,23 @@
+package com.example.myfirstkotlinapp.data
+
+import android.content.Context
+import androidx.room.AutoMigration
+import androidx.room.Database
+import androidx.room.Room.databaseBuilder
+import androidx.room.RoomDatabase
+
+@Database(entities = arrayOf(Movie::class), version = 1,)
+abstract class MovieDatabase : RoomDatabase() {
+    abstract fun movieDao() : MovieDao
+
+    companion object{
+        private var INSTANCE : MovieDatabase? = null
+        // TODO MAIN THREAD QUERIE REMOVE
+        fun getAppDatabase(context: Context) : MovieDatabase? {
+            if( INSTANCE == null){
+                INSTANCE = databaseBuilder(context.applicationContext, MovieDatabase::class.java, "MovieDatabase").allowMainThreadQueries().build()
+            }
+            return INSTANCE
+        }
+    }
+}
